4d4eb7de1be448cf5dd06dfee78dee0aa09bd630
Akib Hasan <ahratul740@gmail.com>
2022-10-14 19:28:40 +0200

    update

--- app.py
+++ app.py
@@ -1,51 +1,73 @@
 import streamlit as st
-import json
 import requests
-import numpy as np
 import matplotlib.pyplot as plt
 import json
 import tensorflow as tf
 import numpy as np
-
 from flask import Flask, request
 
-app = Flask(__name__)
+if not hasattr(st, 'already_started_server'):
+    # Hack the fact that Python modules (like st) only load once to
+    # keep track of whether this file already ran.
+    st.already_started_server = True
 
-model = tf.keras.models.load_model('model.h5')
-feature_model = tf.keras.models.Model(
-    model.inputs,
-    [layer.output for layer in model.layers]
-)
+    st.write('''
+        The first time this script executes it will run forever because it's
+        running a Flask server.
 
-_, (x_test, _) = tf.keras.datasets.mnist.load_data()
-x_test = x_test / 255
-
-
-def get_prediction():
-    index = np.random.choice(x_test.shape[0])
-    image = x_test[index, :, :]
-    image_arr = np.reshape(image, (1, 784))
-    return feature_model.predict(image_arr), image
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        preds, image = get_prediction()
-        final_preds = [p.tolist() for p in preds]
-        return json.dumps({
-            'prediction': final_preds,
-            'image': image.tolist()
-        })
-
-    return 'Welcome To The Model Server!'
-
-
-if __name__ == '__main__':
-    app.run()
+        Just close this browser tab and open a new one to see your Streamlit
+        app.
+    ''')
 
 
 
+    app = Flask(__name__)
+
+    model = tf.keras.models.load_model('model.h5')
+    feature_model = tf.keras.models.Model(
+        model.inputs,
+        [layer.output for layer in model.layers]
+    )
+
+    _, (x_test, _) = tf.keras.datasets.mnist.load_data()
+    x_test = x_test / 255
+
+
+    def get_prediction():
+        index = np.random.choice(x_test.shape[0])
+        image = x_test[index, :, :]
+        image_arr = np.reshape(image, (1, 784))
+        return feature_model.predict(image_arr), image
+
+
+    @app.route('/', methods=['GET', 'POST'])
+    def index():
+        if request.method == 'POST':
+            preds, image = get_prediction()
+            final_preds = [p.tolist() for p in preds]
+            return json.dumps({
+                'prediction': final_preds,
+                'image': image.tolist()
+            })
+
+        return 'Welcome To The Model Server!'
+
+
+    if __name__ == '__main__':
+        app.run()
+
+    # @app.route('/foo')
+    # def serve_foo():
+    #     return 'This page is served via Flask!'
+    #
+    # app.run(port=8888)
+
+
+# We'll never reach this part of the code the first time this file executes!
+
+# Your normal Streamlit app goes here:
+x = st.slider('Pick a number')
+st.write('You picked:', x)
 
 
 URI = 'http://127.0.0.1:5000'
